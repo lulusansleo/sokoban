066922b0fa30e0b6fd749476d0db0353c9555511
lulusansleo <luan.bourbier@epitech.eu>
2022-11-22 19:52:22 +0100

    [fix] strncpy '\0' problem

--- lib/my/my_strncpy.c
+++ lib/my/my_strncpy.c
@@ -11,7 +11,6 @@ char *my_strncpy(char *dest, char const *src, int n)
 {
     for (int i = 0; i < n && src[i] != '\0'; i++)
         dest[i] = src[i];
-    if (n > my_strlen(src))
-        dest[n] = '\0';
+    dest[n] = '\0';
     return dest;
 }
